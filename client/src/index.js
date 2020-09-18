10b156bb1c77f0c4054bd8a04db504991fd39af0
Ritesh Patil <riteshsp2000@gmail.com>
2020-09-18 12:10:51 +0530

    Add redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './views/App';
+import Root from './Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Root>
+    <App />
+  </Root>,
+  document.getElementById('root')
+);
